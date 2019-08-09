4befcc5b0ca248469e437998f5de69b6899476e6
zzzcq <1270934223qq.com>
2019-08-09 15:02:39 +0800

    create feature-2.0-rollframe branch

--- framework/roll/src/main/java/com/webank/ai/eggroll/framework/roll/helper/SessionHelper.java
+++
@@ -1,9 +0,0 @@
-package com.webank.ai.eggroll.framework.roll.helper;
-
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-
-@Component
-@Scope("prototype")
-public class SessionHelper {
-}
